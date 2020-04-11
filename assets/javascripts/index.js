1a817ae4e1c09130065ab3d6c25811233d40bdd9
whypam <@users.noreply.github.com>
2020-04-10 22:57:56 -0700

    Homepage
    Homepage coding first version

---
+++ assets/javascripts/index.js
@@ -0,0 +1,20 @@
+function play() {
+  console.log("!@")
+  let playBtn = document.getElementById("playBtn")
+  let mainImage = document.getElementById("mainImage")
+  let mainVideo = document.getElementById("mainVideo")
+
+  if (mainVideo.paused) {
+    playBtn.src = "assets/images/close.png"
+    mainImage.style.display = "none"
+    mainVideo.style.display = "block"
+    mainVideo.play()
+    didPlay = true
+  } else {
+    mainVideo.pause()
+    playBtn.src = "assets/images/playBtn.png"
+    mainImage.style.display = "block"
+    mainVideo.style.display = "none"
+    didPlay = false
+  }
+}
